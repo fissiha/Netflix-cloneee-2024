ea6c3c9b015fd735ef321f23ced2704e60b63b9e
fissiha <162886060+fissiha@users.noreply.github.com>
2024-05-10 19:21:34 -0700

    final

--- netflix-cloneee/src/Components/Header/Header.js
+++ netflix-cloneee/src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './header.css'
+import NetflixLogo from "../../assets/images/NetflixLogo.png"
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
@@ -12,9 +13,9 @@ const Header = () => {
     <div className='header-outer-container'>
       <div className='header-container'>
         <div className='header-left'>
-            <ul>
-                {/* <li> <img src={NetflixLogo} alt='NetflixLogo' width='100'> </li> */}
-                <li>Netflix</li>
+            <ul className='header-left-ul'>
+            <li><img src={NetflixLogo} alt="Netflix Logo" width="100" /></li>
+                {/* <li>Netflix</li> */}
                 <li>Home</li>
                 <li>TvShows</li>
                 <li>Movies</li>
